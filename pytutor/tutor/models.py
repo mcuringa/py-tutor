312fdfbf40e06b8f13e7ed8491c386a3d299f1ff
mxc <matt@curinga.com>
2014-02-10 14:02:29 -0500

    checking in initial Django stuff

---
+++ pytutor/tutor/models.py
@@ -0,0 +1,61 @@
+# tutor.models.py
+# by: mxc
+"""
+Define the Django models for the
+PyTutor application. Defines
+key objects: Question.
+"""
+
+from django.db import models
+from django import forms
+from django.forms import ModelForm
+
+
+
+class Question(models.Model):
+
+    level_choices = [(i,i) for i in range(1,11)]
+
+    prompt = models.TextField
+    function_name = models.CharField
+    hint = models.TextField(blank=True)
+    test = models.TextField(blank=True)
+    solution = models.TextField(blank=True)
+    level = models.IntegerField(choices=level_choices)
+    modified = models.DateTimeField(auto_now=True)
+
+class Tag(models.Model):
+    tag = models.CharField
+
+class TestData(models.Model):
+    params = models.CharField
+    result = models.CharField()
+    error = models.CharField(blank=True)
+    fail_msg = models.TextField(blank=True)
+    success_msg = models.TextField(blank=True)
+
+
+
+
+class QuestionForm(ModelForm):
+    class Meta:
+        model = Question
+    exclude = ["modified"]
+
+
+
+class Response():
+
+    def check(self, response):
+        f = eval(response)
+        result = f(self.params)
+        if result == self.result:
+            return (True, Question.success)
+        return (False, Question.fail)
+
+
+
+
+
+
+
---
+++ pytutor/tutor/urls.py
@@ -0,0 +1,19 @@
+# tutor.urls.py
+# by: mxc
+"""
+Define the mappings for URLs in
+the tutor app.
+"""
+
+from django.conf.urls import patterns, include, url
+
+import views
+
+urlpatterns = patterns('',
+    url(r'^$', views.quest_list),
+    url(r'^new$', views.quest_form),
+    url(r'^(?P<pk>[0-9]?)[/]$', views.app_form),
+    url(r'^([0-9]?)/del[/]$', views.del_app),
+    url(r'^save_app$', views.save_app),
+)
+
