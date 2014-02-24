5f47e9340e71f4d6eaa33f08405a1370d9c6f699
mxc <matt@curinga.com>
2014-02-24 16:42:49 -0500

    more robust data model with Response, Flags, etc.

--- pytutor/tutor/models.py
+++ pytutor/tutor/models.py
@@ -9,53 +9,110 @@ key objects: Question.
 from django.db import models
 from django import forms
 from django.forms import ModelForm
+from django.contrib.auth.models import User
 
 
 class Question(models.Model):
+    """A Question is a coding challenge for the studier.
+    It provides them with a prompt and the name of the
+    function they must write to solve the problem. Questions
+    have a difficulty level and an arbitrary set of Tags 
+    that categorize them. Questions are meant to be edited
+    wiki-style -- wtih many editors boldly making changes, but
+    with a clear revision history and easy system for rolling them
+    back."""
 
     level_choices = [(i,i) for i in range(1,11)]
 
     prompt = models.TextField()
     function_name = models.CharField(max_length=300)
     hint = models.TextField(blank=True)
-    test = models.TextField(blank=True)
     solution = models.TextField(blank=True)
     level = models.IntegerField(choices=level_choices)
-    modified = models.DateTimeField(auto_now=True)
     tags = []
+    version = models.IntegerField()
+    comment = models.CharField(max_length=500)
+    modified = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField()
+    creator = models.ForeignKey(User)
+    modifier = models.ForeignKey(User)
+    
 
 class QuestionForm(ModelForm):
     class Meta:
         model = Question
-    exclude = ["modified"]
+    exclude = ["modified", "created", "creator", "modifier", "version"]
 
-class Response(models.Model):
 
-    program = models.TextField(blank=True)
-    question = models.ForeignKey(Question)
-    modified = models.DateTimeField(auto_now=True)
+class ArchiveQuestion(Question):
+    """An ArchiveQuestion is created everytime a
+    Question is updated. ArchiveQuestions can
+    be reviewed and reverted to. For every Question
+    there may be serveral ArchiveQuestions. Users
+    Responses are linked to ArchiveQuestions, as 
+    are Flags. Comments are linked to the current
+    Question, because, generally, comments will endure
+    across vesions of a Question."""
+
+    archived = models.DateTimeField(auto_now=True)
     
-    #check needs editing
-    def check(self, response):
-        f = eval(response)
-        result = f(self.params)
-        if result == self.result:
-            return (True, Question.success)
-        return (False, Question.fail)
+
 
 class Tag(models.Model):
+    """Tags are the set of case-insensitive tags
+    that are applied to Questions in the tutoring
+    system."""
+
     tag = models.CharField(max_length=300)
 
-class TestData(models.Model):
-    params = models.CharField(max_length=500)
+
+class Test(models.Model):
+    """
+        Each Question must have at leat one Test
+        before it can be published. Tests are meant
+        to be dynamically executed by the system--
+        each test has arguments to pass to the Users's
+        function and the expected result. If all
+        Question tests pass, the Response is considered correct.
+    """
+    args = models.CharField(max_length=500)
     result = models.TextField()
-    error = models.CharField(max_length=500, blank=True)
     fail_msg = models.TextField(blank=True)
     success_msg = models.TextField(blank=True)
 
 
+class Response(models.Model):
+    """A User's attempt to answer a Question
+    is captured in the Response. The Response
+    is evaluated against the Tests for a Question.
+    If all tests are correctly passed, the Response
+    is marked correct"""
+
+    code = models.TextField(blank=True)
+    is_correct = models.BooleanField(default=False)
+    attempt = models.IntegerField()
+    submitted = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User)
+    question = models.ForeignKey(ArchiveQuestion)
 
 
+class ResponseForm(ModelForm):
+    class Meta:
+        model = Response
+    exclude = ["submitted", "user", "is_correct", "attempt"]
+
+
+class QuestionFlag(models.Model):
+    """    """
+    flags = [ (1, "Unclear"),
+          (2, "Too Hard"),
+          (3, "Too Easy"),
+          (4, "Innapropriate")]
+
+    flag = models.IntegerField(choices=flags)
+    question = models.ForeignKey(ArchiveQuestion)
+    creator = models.ForeignKey(User)
+    created = models.DateTimeField(auto_now=True)
 
 
 
