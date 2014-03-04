fa928a147d1a97a5b81b4d8dc9102a864e38206b
Hannah Groves <hsgroves@gmail.com>
2014-03-04 16:57:22 -0500

    Beginning Response Model

--- pytutor/tutor/views.py
+++ pytutor/tutor/views.py
@@ -1,3 +1,4 @@
+import random
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 
@@ -6,11 +7,31 @@ from tutor.models import *
 
 def study(request):
     """Choose the next question for the user to study."""
-    return render(request, 'tutor/study.html')
+    
+    question = random.choice(Question.objects.all())
+    context = {"question": question}
+    return render(request, 'tutor/study.html', context)
 
+def respond(request, pk):
+    """Allow user to write a response to a question."""
+    
+    pk = int(request.GET["pk"])
+    question = Question.objects.get(pk=pk)
+    context = {"question": question}
+    
+    return render(request, 'tutor/respond.html', context)
+
+def submit_response(request, qpk, rpk):
+	"""Submit user's response for evaluation."""
+	
+	question = Question.objects.get(pk=qpk)
+	response = Response.objects.get(pk=rpk)
+	context = {"question" : question, "response" : response}
+	#needs form stuff too
+	return render(request, 'tutor/feedback.html', context)
 
 def list(request):
-    """List the questions in the databse"""
+    """List the questions in the database"""
     
     questions = Question.objects.all()
     context = {"questions": questions}
