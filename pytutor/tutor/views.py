2f0897bf33f95b9684a4cb67d27f2d5202f8b376
mxc <matt@curinga.com>
2014-03-05 18:38:14 -0500

    fixed question_form view so it works with tests

--- pytutor/tutor/views.py
+++ pytutor/tutor/views.py
@@ -24,17 +24,21 @@ def question_form(request, pk=0):
     if pk == 0:
         form = QuestionForm()
         history = []
+        tests = []
     else:
         question = Question.objects.get(pk=pk)
         form = QuestionForm(instance=question)
         history = ArchiveQuestion.objects.all().filter(parent_id=pk)
         tests = Test.objects.all().filter(question=question)
-        test_form = TestForm()
+
+
+    test_form = TestForm()
 
     context = { "question": form,
                 "pk": pk,
                 "history": history,
-                "test_form": test_form
+                "test_form": test_form,
+                "tests": tests
               }
 
     return render(request, 'tutor/question_form.html', context)
