2d1d8b4a2453e3258d1e49f91fce96f8506568d6
mxc <matt@curinga.com>
2014-03-09 00:24:36 -0500

    added jquery form submit to save unit test data as ajax request

--- pytutor/static/js/tutor.js
+++ pytutor/static/js/tutor.js
@@ -1,20 +1,24 @@
 
 
-// Attach a submit handler to the form
-$( "#test-form" ).submit(function( event ) 
-{
-    // Stop form from submitting normally
-    event.preventDefault();
-    var $form = $( this );
-    var data = $form.serialize();   
-    var url = $form.attr( "action" );
+$( document ).ready(function() {
+    
+    console.log($( "#test-form" ));
+    
+    $( "#test-form" ).submit(function( event ) 
+    {
+        console.log('submitting unit test');
 
-    var posting = $.post( url, data );
-    // Put the results in a div
-    posting.done(function( data ) {
-        var content = $( data ).find( "#content" );
-        $( "#result" ).empty().append( content );
+        // Stop form from submitting normally
+        event.preventDefault();
+        var $form = $( this );
+        var data = $form.serialize();   
+        var url = $form.attr( "action" );
+
+        var posting = $.post( url, data );
+        
+        posting.done(function( data ) {
+            $( "#unit-tests ul" ).append( "<li>" + data + "</li>" );
+        });
     });
 });
-
 
