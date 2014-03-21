c20a276be36aa300883c00e6b7734bfe9060d187
mxc <matt@curinga.com>
2014-03-20 21:00:58 -0400

    begining of a student model

---
+++ pytutor/tutor/user_models.py
@@ -0,0 +1,11 @@
+# tutor.models.py
+# by: mxc
+
+from django.db import models
+from django import forms
+from django.forms import ModelForm
+from django.contrib.auth.models import User
+
+import models
+
+
