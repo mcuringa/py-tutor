c14beaaaa5dca5cb0b2f608280b5e629943c7dfb
mxc <matt@curinga.com>
2014-04-03 22:47:08 -0400

    Changes were made to add the 'ace' online text editor.
    - added ace code to static/js
    - updated tutor.js to configure the ace editor
    - changed base.html to include ace javascript
    - changed main.css to style the ace editor for user study/response form
    - changed respond template to include the ace editor

--- pytutor/static/js/tutor.js
+++ pytutor/static/js/tutor.js
@@ -1,6 +1,19 @@
 
 
+function configureEditor(editorId)
+{
+    // configure ace editor
+    var editor = ace.edit(editorId);
+    editor.setTheme("ace/theme/GitHub");
+    editor.getSession().setMode("ace/mode/python");
+    editor.getSession().setUseSoftTabs(true);
+    return editor
+}
+
+
 $( document ).ready(function() {
+
+    var responseEditor = configureEditor("response-editor");
     
     $( "#test-form" ).submit(function( event ) 
     {
@@ -20,12 +33,15 @@ $( document ).ready(function() {
 
     $("#response-form").submit(function ( event) 
     {
+        console.log("response form submitted");
         // Stop form from submitting normally
         event.preventDefault();
         var $form = $( this );
-        var code = $("id_code").html();
+        
+        var code = responseEditor.getValue();
+        console.log(code);
+        $("#user_code").val(code);
         var data = $form.serialize();
-        data.push(code);
         var url = $form.attr( "action" );
 
         $.post( url, data );
