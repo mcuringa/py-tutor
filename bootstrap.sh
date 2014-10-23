b7196f3950b3bc92c07cfd93ff0b0b72bd048b44
mxc <matt@curinga.com>
2014-10-23 02:05:33 -0400

    added bootstrap locally to project with helper script to build it with grunt

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+cd res/bootstrap-3.2.0/;
+grunt dist;
+cd ../..;
+cp res/bootstrap-3.2.0/dist/css/bootstrap.min.css pytutor/static/css/;
+cp res/bootstrap-3.2.0/dist/css/bootstrap pytutor/static/css/;
+cp res/bootstrap-3.2.0/dist/css/bootstrap-theme.min.css pytutor/static/css/;
+cp res/bootstrap-3.2.0/dist/js/bootstrap.min.js pytutor/static/js/;
