773e2c6a732467e725cf87b68abda3038e7ad0db
mxc <matt@curinga.com>
2015-03-16 20:46:23 -0400

    working on socialprofile migrations

---
+++ pytutor/social/migrations/0002_create_profiles.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+from django.contrib.auth.models import User
+
+class Migration(migrations.Migration):
+
+
+    dependencies = [("social", "0001_initial")]
+
+
+    def create_profiles(apps, schema_editor):
+        SocialProfile = apps.get_model("social", "SocialProfile")
+        users = User.objects.all()
+        for user in users: 
+            SocialProfile.objects.create(user=user)
+
+
+    operations = [
+        migrations.RunPython(create_profiles)
+    ]
